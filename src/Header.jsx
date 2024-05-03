81c2f4601adec3e683ad957c0a62786a04b6f698
Alexander Agu <theonlyagu@gmail.com>
2024-05-03 23:54:27 +0200

    Styling my responsive navigation bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,10 +10,17 @@ function Header(){
                 </a>
             </div>
 
+            <input type="checkbox" id='sidebar-active'/>
+                <label htmlFor="sidebar-active" className='open-sidebar-btn'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+            </label>
+
+                
+            <label htmlFor="sidebar-active" className='overlay'></label>
             
             <nav className='navbar'>
 
                 <ul className='nav-list'>
+                    <label htmlFor="sidebar-active" className='close-sidebar-btn'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></label>
                     <li className='nav-items'>
                         <a href="#" className='nav-links'>Home</a>
                     </li>
